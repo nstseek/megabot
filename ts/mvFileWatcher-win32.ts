c89f59b11d8a483d17be1f475b941aff456e5b8c
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-05-23 23:18:03 -0300

    mvFileWatcher win32 update

---
+++ ts/mvFileWatcher-win32.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+import * as childProcess from 'child_process';
+
+var counter = 1;
+var internalCounter = 0;
+var lastFilename: string;
+try {
+    childProcess.execSync(`mkdir watchDir`, { stdio: `inherit` });   
+} catch (error) {
+    
+}
+fs.watch(`watchDir`, (event, filename) => {
+    if(event == `change` && filename.search(/(.*).html/) != -1 && filename != lastFilename){
+        lastFilename = filename;
+        console.log(`${filename} added`);
+        let counterString = String(counter);
+        if(counter < 10) counterString = `0${counterString}`;
+        try {
+            childProcess.execSync(`mv -v "${filename}" ${counterString}.html`, { stdio: 'inherit', cwd: 'watchDir' });   
+        } catch (error) {
+            
+        }
+        counter++;
+    }
+});
+console.log(`Listening for changes on ./watchDir/...`);
