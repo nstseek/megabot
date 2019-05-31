7ffe03639bf4c12d83ab672f019dd8e97541e981
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-05-31 18:20:10 -0300

    fix mvfilewatcher up to 999

--- ts/mvFileWatcher.ts
+++ ts/mvFileWatcher.ts
@@ -2,15 +2,16 @@ import * as fs from 'fs';
 import * as childProcess from 'child_process';
 
 var counter = 1;
-var internalCounter = 0;
 var lastFilename: string;
 childProcess.execSync(`mkdir -pv watchDir`, { stdio: `inherit` });
 fs.watch(`watchDir`, (event, filename) => {
     if(event == `change` && filename.search(/(.*).html/) != -1 && filename != lastFilename){
         lastFilename = filename;
         console.log(`${filename} added`);
-        let counterString = String(counter);
-        if(counter < 10) counterString = `0${counterString}`;
+        let counterString: string;
+        if (counter < 10) counterString = `00${counter}`;
+        else if (counter < 100 && counter >= 10) counterString = `0${counter}`;
+        else counterString = `${counter}`;
         try {
             childProcess.execSync(`mv -v "${filename}" ${counterString}.html`, { stdio: 'inherit', cwd: 'watchDir' });
             childProcess.execSync(`cp -v ${counterString}.html ../futureHTMLs/`, { stdio: 'inherit', cwd: 'watchDir' });
